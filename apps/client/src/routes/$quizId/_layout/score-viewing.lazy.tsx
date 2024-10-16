b2537270d7e8240b29d218cb086e3aabd1242d7c
Ilya <elijahmg25@gmail.com>
2024-10-16 18:47:50 +0200

    feat: score viewing

--- apps/client/src/routes/$quizId/_layout/score-viewing.lazy.tsx
+++ apps/client/src/routes/$quizId/_layout/score-viewing.lazy.tsx
@@ -1,5 +1,20 @@
 import { createLazyFileRoute } from '@tanstack/react-router'
+import { useTeamData } from '@/hooks/use-team-data';
+import { CenterWrapper } from '@/components/wrappers/center-wrapper';
+import { TeamInGameHeader } from '@/components/modules/team-in-game-header';
 
 export const Route = createLazyFileRoute('/$quizId/_layout/score-viewing')({
-  component: () => <div>Hello /$quizId/_layout/score-viewing!</div>,
+  component: ScoreView,
 })
+
+function ScoreView() {
+  const { teamId } = useTeamData()
+
+  return (
+    <CenterWrapper>
+      <TeamInGameHeader/>
+      <div>Hello /$quizId/_layout/score-viewing!</div>
+
+    </CenterWrapper>
+  )
+}
